755238fd2cfebfe141234af64273fe3be7f07f8f
gui-sousa <72104673+gui-sousa@users.noreply.github.com>
2021-10-13 16:11:55 -0300

    Erro de dependência na instalação do Jenkins
    Resolvido

--- pacotes_jenkins.sh
+++ pacotes_jenkins.sh
@@ -4,6 +4,11 @@ sudo yum -y install java-1.8.0-openjdk-devel
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 
+#Resolve dependências daemonize
+
+curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
+sudo rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
+
 #Instalação Jekins
 
 sudo yum -y install jenkins --nogpgcheck 
